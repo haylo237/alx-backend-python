08271010a445d7bdde36475e3893e9a1cb445178
Haylo237 <njikeabednego@hotmail.com>
2024-10-11 00:54:30 +0100

    corrected task 0

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 """Basic annotations - add"""
+
 def add(a: float, b: float) -> float:
-    """ Takes 2 float and return the sum as float"""
+    """ Takes 2 float and returns the sum as float"""
     return a + b
